c51ee15061fc990d67a93f1da0f4bdaf7d06d98d
guy natan <guynatan@gmail.com>
2020-02-25 11:34:07 +0200

    config

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Text
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 		config.i18n.default_locale = :he
+    # config.i18n.fallbacks = [I18n.default_locale] 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
