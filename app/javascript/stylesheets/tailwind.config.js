2363291377bba6d43280db330d8f03fab4f74d2b
guy natan <guynatan@gmail.com>
2020-08-02 15:48:52 +0300

    no purgecss

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -2,11 +2,11 @@ module.exports = {
   prefix: '',
   important: false,
   separator: ':',
-  purge: [
-    './app/**/*.html.erb',
-   './app/helpers/**/*.rb',
-   './app/javascript/**/*.js',
-  ],
+  // purge: [
+  //   './app/**/*.html.erb',
+  //  './app/helpers/**/*.rb',
+  //  './app/javascript/**/*.js',
+  // ],
   theme: {
     colors: {
       transparent: 'transparent',
